8eb8fdb6c14eab3fbf2a69e756ff91edc799f355
blueblood74 <blueblood74@hotmail.com>
2026-07-03 08:49:11 +0900

    Initial Commit

---
+++ java/src/main/java/Main.java
@@ -0,0 +1,27 @@
+package jp.co.training;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        String url = "jdbc:oracle:thin:@oracle:1521/FREEPDB1";
+        String user = "system";
+        String password = "Oracle123";
+
+        try {
+            Connection con = DriverManager.getConnection(url, user, password);
+
+            System.out.println("接続成功！");
+            System.out.println(con);
+
+            con.close();
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+}
